32340bf3c8f647ada7c602378e0820755403d61d
Kenji Ohtsuka <kok.fdcm@gmail.com>
2016-12-23 22:24:48 +0900

    delete comments

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,5 @@ require 'core/ClassLoader.php';
 
 $loader = new \core\ClassLoader();
 $loader->registerDir(__DIR__);
-#$loader->registerDir(__DIR__ . '/core');
-#$loader->registerDir(__DIR__ . '/models');
 $loader->register();
 
